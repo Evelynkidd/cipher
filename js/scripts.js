0dc2b39f2b2a6a5ddaf8a8036bd726128df1ec2f
Evelyn Fenner-Dorrity and Linda Luu <linda@>
2017-08-07 16:32:57 -0700

    capitalize the first and last letter and return as a string, with the prompted sentence

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// var string = prompt("Give me a sentence")
+//
+// function capitalize(string) {
+//   return (string.charAt(0).toUpperCase() + string.charAt(string.length-1).toUpperCase());
+// }
+//
+// alert(capitalize(string))
+
+
+var string = prompt("Give me a sentence")
+
+function capitalize(string) {
+  return (string.charAt(0).toUpperCase() + string.charAt(string.length-1).toUpperCase());
+}
+
+function reverse(newstring) {
+  return newstring;
+}
+
+alert(newstring)
